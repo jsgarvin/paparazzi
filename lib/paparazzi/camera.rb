e13207530b55330b307ee7b5159adc8fff4c1cc8
Jonathan Garvin <jon@5valleys.com>
2011-05-22 15:01:45 -0600

    basic initialization working

--- lib/paparazzi/camera.rb
+++ lib/paparazzi/camera.rb
@@ -1,11 +1,63 @@
 require 'admit_one'
+require 'yaml'
 
 module Paparazzi
   class Camera
     class << self
-      def trigger
-        return true
+      FREQUENCIES = [:hourly,:daily,:weekly,:monthly,:yearly]
+      REQUIRED_SETTINGS = [:source,:destination]
+      attr_accessor :source, :destination, :rsync_flags
+      
+      def trigger(settings = {})
+        validate_and_cache_settings(settings)
+        AdmitOne::LockFile.new(:paparazzi) do
+          initialize
+        end
+        
+      end
+      
+      #######
+      private
+      #######
+      
+      def validate_and_cache_settings(settings)
+        [:source,:destination,:rsync_flags].each do |setting_name|
+          if REQUIRED_SETTINGS.include?(setting_name) and settings[setting_name].nil?
+            raise MissingSettingError, "#{setting_name} is required"
+          else
+            self.send("#{setting_name}=",settings[setting_name])
+          end
+        end
+        
+        raise MissingFolderError, source unless File.exist?(source) and File.directory?(source)
+        raise MissingFolderError, destination unless File.exist?(destination) and File.directory?(destination)
+      end
+      
+      def initialize
+        @previous_target_name = {}
+        FREQUENCIES.each do |frequency|
+          Dir.mkdir(destination(frequency)) unless File.exists?(destination(frequency)) && File.directory?(destination(frequency))
+        
+          full_path = Dir[destination(frequency)+'/*'].sort{|a,b| File.ctime(b) <=> File.ctime(a) }.first
+          @previous_target_name[frequency] = full_path ? File.basename(full_path) : ''
+        end
+        
+        if @previous_target_name[:hourly] != last_successful_hourly_target and !last_successful_hourly_target.nil? and File.exists?(destination(:hourly) + '/' + last_successful_hourly_target)
+          File.rename(previous_target(:hourly), current_target(:hourly))
+          @previous_target_name[:hourly] = last_successful_hourly_target
+        end
+      end
+      
+      def destination(frequency = nil)
+        frequency.nil? ? @destination : "#{@destination}/#{frequency}"
+      end
+      
+      def last_successful_hourly_target
+        @last_successful_hourly_target ||= File.exists?("#{destination}/.paparazzi.yml") ? YAML.load_file("#{destination}/.paparazzi.yml")[:last_successful_snapshot] : nil
       end
     end
   end
+  
+  class MissingSettingError < StandardError; end
+  class MissingFolderError < StandardError; end
 end
--- test/unit/camera_test.rb
+++ test/unit/camera_test.rb
@@ -3,8 +3,36 @@ require 'test/unit'
 
 class CameraTest < Test::Unit::TestCase
   
-  def test_trigger_camera
-    assert Paparazzi::Camera.trigger
+  def test_should_raise_exception_on_missing_required_settings
+    assert_raise(Paparazzi::MissingSettingError) { Paparazzi::Camera.trigger({}) }
+  end
+  
+  def test_should_store_valid_settings_in_attr_accessors
+    test_settings = {
+      :source => File.expand_path('../../source',  __FILE__),
+      :destination => File.expand_path('../../destination',  __FILE__),
+      :rsync_flags => '-L'
+    }
+    Paparazzi::Camera.trigger(test_settings)
+    [:source,:destination,:rsync_flags].each do |setting_name|
+      assert_equal(test_settings[setting_name],Paparazzi::Camera.send(setting_name),setting_name)
+    end
+  end
+  
+  def test_should_raise_exception_on_missing_source_or_destination_folder
+    test_settings = {
+      :source => File.expand_path('../../missing_source',  __FILE__),
+      :destination => File.expand_path('../../destination',  __FILE__),
+      :rsync_flags => '-L'
+    }
+    assert_raise(Paparazzi::MissingFolderError) { Paparazzi::Camera.trigger(test_settings) }
+    
+    test_settings = {
+      :source => File.expand_path('../../source',  __FILE__),
+      :destination => File.expand_path('../../missing_destination',  __FILE__),
+      :rsync_flags => '-L'
+    }
+    assert_raise(Paparazzi::MissingFolderError) { Paparazzi::Camera.trigger(test_settings) }
   end
   
 end

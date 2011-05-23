47896bb7075543381f9084b5acd8e0cbaac3b798
Jonathan Garvin <jon@5valleys.com>
2011-05-22 19:48:13 -0600

    all core code in place and tests passing

--- lib/paparazzi/camera.rb
+++ lib/paparazzi/camera.rb
@@ -1,17 +1,21 @@
 require 'admit_one'
 require 'yaml'
+require 'fileutils'
 
 module Paparazzi
   class Camera
+    FREQUENCIES = [:hourly,:daily,:weekly,:monthly,:yearly]
+    REQUIRED_SETTINGS = [:source,:destination]
+      
     class << self
-      FREQUENCIES = [:hourly,:daily,:weekly,:monthly,:yearly]
-      REQUIRED_SETTINGS = [:source,:destination]
       attr_accessor :source, :destination, :rsync_flags
       
       def trigger(settings = {})
         validate_and_cache_settings(settings)
         AdmitOne::LockFile.new(:paparazzi) do
           initialize
+          purge_old_snapshots
+          make_snapshots
         end
         
       end
@@ -34,17 +38,17 @@ module Paparazzi
       end
       
       def initialize
-        @previous_target_name = {}
+        @previous_snapshot_name = {}
         FREQUENCIES.each do |frequency|
           Dir.mkdir(destination(frequency)) unless File.exists?(destination(frequency)) && File.directory?(destination(frequency))
         
           full_path = Dir[destination(frequency)+'/*'].sort{|a,b| File.ctime(b) <=> File.ctime(a) }.first
-          @previous_target_name[frequency] = full_path ? File.basename(full_path) : ''
+          @previous_snapshot_name[frequency] = full_path ? File.basename(full_path) : ''
         end
         
-        if @previous_target_name[:hourly] != last_successful_hourly_target and !last_successful_hourly_target.nil? and File.exists?(destination(:hourly) + '/' + last_successful_hourly_target)
+        if @previous_snapshot_name[:hourly] != last_successful_hourly_snapshot and !last_successful_hourly_snapshot.nil? and File.exists?(destination(:hourly) + '/' + last_successful_hourly_snapshot)
           File.rename(previous_target(:hourly), current_target(:hourly))
-          @previous_target_name[:hourly] = last_successful_hourly_target
+          @previous_snapshot_name[:hourly] = last_successful_hourly_snapshot
         end
       end
       
@@ -52,9 +56,63 @@ module Paparazzi
         frequency.nil? ? @destination : "#{@destination}/#{frequency}"
       end
       
-      def last_successful_hourly_target
-        @last_successful_hourly_target ||= File.exists?("#{destination}/.paparazzi.yml") ? YAML.load_file("#{destination}/.paparazzi.yml")[:last_successful_snapshot] : nil
+      def last_successful_hourly_snapshot
+        @last_successful_hourly_snapshot ||= File.exists?("#{destination}/.paparazzi.yml") ? YAML.load_file("#{destination}/.paparazzi.yml")[:last_successful_snapshot] : nil
       end
+      
+      def last_successful_hourly_snapshot=(string)
+        cached_data ||= File.exists?("#{destination}/.paparazzi.yml") ? YAML.load_file("#{destination}/.paparazzi.yml") : {}
+        cached_data[:last_successful_snapshot] = string
+        File.open("#{destination}/.paparazzi.yml", 'w') {|file| file.write(cached_data.to_yaml) }
+      end
+    
+      def purge_old_snapshots
+        keepers = {:hourly => 24, :daily => 7, :weekly => 5, :monthly => 12}
+        keepers.keys.each do |frequency|
+          while Dir[destination(frequency)+'/*'].size > keepers[frequency]-1
+            full_path = Dir[destination(frequency)+'/*'].sort{|a,b| File.ctime(a) <=> File.ctime(b) }.first
+            FileUtils.rm_rf(full_path)
+          end
+        end
+      end
+
+      def make_snapshots
+        FREQUENCIES.each do |frequency|
+          Dir.mkdir(current_snapshot(frequency)) unless File.exists?(current_snapshot(frequency))
+          if frequency == :hourly and previous_snapshot_name(frequency) == ''
+            system 'rsync', *(['-aq', '--delete'] + [rsync_flags] + [source, current_snapshot(frequency)])          
+          elsif previous_snapshot_name(frequency) != current_snapshot_name(frequency)
+            system 'rsync', *(['-aq', '--delete', "--link-dest=#{link_destination(frequency)}"] + [rsync_flags] + [source, current_snapshot(frequency)])
+          end
+        end
+        unless previous_snapshot_name(:hourly) == current_snapshot_name(:hourly)
+          self.last_successful_hourly_snapshot = current_snapshot_name(:hourly)
+        end
+      end
+      
+      def current_snapshot(frequency)
+        destination(frequency) + '/' + current_snapshot_name(frequency)
+      end
+      
+      def current_snapshot_name(frequency)
+        @start_time ||= Time.now #lock in time so that all results stay consistent over long runs
+        case frequency
+          when :hourly  then @start_time.strftime('%Y-%m-%d.%H')
+          when :daily   then @start_time.strftime('%Y-%m-%d')
+          when :weekly  then sprintf("%04d-%02d-week-%02d", @start_time.year, @start_time.month, (@start_time.day/7))
+          when :monthly then @start_time.strftime("%Y-%m")
+          when :yearly  then @start_time.strftime("%Y")
+        end
+      end
+      
+      def previous_snapshot_name(frequency)
+        @previous_snapshot_name[frequency]
+      end
+      
+      def link_destination(frequency)
+        frequency == :hourly ? "../#{previous_snapshot_name(:hourly)}" : "../../hourly/#{current_snapshot_name(:hourly)}"
+      end
+      
     end
   end
   
--- test/unit/camera_test.rb
+++ test/unit/camera_test.rb
@@ -1,38 +1,85 @@
 require File.expand_path('../../../lib/paparazzi',  __FILE__)
 require 'test/unit'
+require 'fileutils'
 
 class CameraTest < Test::Unit::TestCase
+  
+  def setup
+    Paparazzi::Camera::FREQUENCIES.each do |frequency|
+      FileUtils.rm_rf("#{destination}/#{frequency}")
+    end
+    FileUtils.rm_rf("#{destination}/.paparazzi.yml")
+  end
   
   def test_should_raise_exception_on_missing_required_settings
     assert_raise(Paparazzi::MissingSettingError) { Paparazzi::Camera.trigger({}) }
   end
   
   def test_should_store_valid_settings_in_attr_accessors
-    test_settings = {
-      :source => File.expand_path('../../source',  __FILE__),
-      :destination => File.expand_path('../../destination',  __FILE__),
-      :rsync_flags => '-L'
-    }
-    Paparazzi::Camera.trigger(test_settings)
+    Paparazzi::Camera.trigger(default_test_settings)
     [:source,:destination,:rsync_flags].each do |setting_name|
-      assert_equal(test_settings[setting_name],Paparazzi::Camera.send(setting_name),setting_name)
+      assert_equal(default_test_settings[setting_name],Paparazzi::Camera.send(setting_name),setting_name)
     end
   end
   
   def test_should_raise_exception_on_missing_source_or_destination_folder
-    test_settings = {
-      :source => File.expand_path('../../missing_source',  __FILE__),
+    my_test_settings = default_test_settings.merge(:source => File.expand_path('../../missing_folder',  __FILE__))
+    assert_raise(Paparazzi::MissingFolderError) { Paparazzi::Camera.trigger(my_test_settings) }
+    
+    my_test_settings = default_test_settings.merge(:destination => File.expand_path('../../missing_folder',  __FILE__))
+    assert_raise(Paparazzi::MissingFolderError) { Paparazzi::Camera.trigger(my_test_settings) }
+  end
+  
+  def test_should_create_frequency_folders_on_initialization
+    Paparazzi::Camera::FREQUENCIES.each do |frequency|
+      assert(!File.exists?("#{destination}/#{frequency}"))
+    end
+    Paparazzi::Camera.trigger(default_test_settings)
+    Paparazzi::Camera::FREQUENCIES.each do |frequency|
+      assert(File.exists?("#{destination}/#{frequency}"))
+    end
+  end
+  
+  def test_should_make_all_first_snapshots_of_source
+    Paparazzi::Camera.trigger(default_test_settings)
+    Paparazzi::Camera::FREQUENCIES.each do |frequency|
+      assert(Dir["#{destination}/#{frequency}/*"].size > 0,"#{destination}/#{frequency}/ is empty")
+      file = File.open(%Q{#{Dir["#{destination}/#{frequency}/*"].first}/test.txt})
+      assert_equal('This is a test, this is only a test.',file.gets)
+    end
+  end
+  
+  def test_should_write_last_successful_snapshot_to_cached_values_file
+    Paparazzi::Camera.trigger(default_test_settings)
+    assert(YAML.load_file("#{destination}/.paparazzi.yml")[:last_successful_snapshot].match(/\d{4}-\d{2}-\d{2}\.\d{2}/))
+  end
+  
+  def test_purges_out_expired_snapshots
+    Dir.mkdir("#{destination}/weekly")
+    (1..5).each do |x|
+      Dir.mkdir("#{destination}/weekly/#{x}")
+      sleep 1;
+    end
+    assert_equal(5,Dir["#{destination}/weekly/*"].size)
+    previous_folder_contents = Dir["#{destination}/weekly/*"]
+    Paparazzi::Camera.trigger(default_test_settings)
+    assert_equal(5,Dir["#{destination}/weekly/*"].size)
+    assert_not_equal(previous_folder_contents,Dir["#{destination}/weekly/*"])
+  end
+  
+  #######
+  private
+  #######
+  
+  def destination
+    @destination ||= File.expand_path('../../destination',  __FILE__)
+  end
+  
+  def default_test_settings
+    @default_test_settings ||= {
+      :source => "#{File.expand_path('../../source',  __FILE__)}/",
       :destination => File.expand_path('../../destination',  __FILE__),
       :rsync_flags => '-L'
     }
-    assert_raise(Paparazzi::MissingFolderError) { Paparazzi::Camera.trigger(test_settings) }
-    
-    test_settings = {
-      :source => File.expand_path('../../source',  __FILE__),
-      :destination => File.expand_path('../../missing_destination',  __FILE__),
-      :rsync_flags => '-L'
-    }
-    assert_raise(Paparazzi::MissingFolderError) { Paparazzi::Camera.trigger(test_settings) }
   end
-  
 end
